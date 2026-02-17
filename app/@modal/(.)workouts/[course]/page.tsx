3b4e3939de7838b3b114231e29f07527802a146a
alexeyasurov <aasurov@gmail.com>
2026-02-17 15:57:21 +0300

    add-api-workout: исправил сложности с входом с разных бразуеров в один акк, сейчас это невозможно

--- app/@modal/(.)workouts/[course]/page.tsx
+++ app/@modal/(.)workouts/[course]/page.tsx
@@ -62,7 +62,7 @@ export default function WorkoutsModalPage({ params }: PageProps) {
                         workouts={workouts}
                         loading={loading}
                         error={error}
-                        onStartWorkout={(workoutId) => router.replace(`/workout/${workoutId}`)}
+                        onStartWorkout={(workoutId) => window.location.replace(`/workout/${workoutId}`)}
                     />
                 </div>
             </div>
